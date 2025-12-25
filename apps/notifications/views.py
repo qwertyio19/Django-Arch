d4d3c94fb92aa4834839a877e783cdf8e23e059f
Saidahmad <djamalbekovsaidahmad@gmail.com>
2025-12-25 17:48:34 +0600

    add_new_api_is_active

--- apps/notifications/views.py
+++ apps/notifications/views.py
@@ -1,4 +1,5 @@
 from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
 from apps.notifications.models import TypeNotification, Notification
 from apps.notifications.serializers import TypeNotificationSerializer, NotificationSerializer
 from apps.notifications.paginations import NotificationPagination
@@ -17,3 +18,14 @@ class NotificationView(mixins.ListModelMixin,
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
     pagination_class = NotificationPagination
+
+    @action(detail=False, methods=['get'], url_path='active')
+    def active_notifications(self, request):
+        active_notifications = Notification.objects.filter(is_active=True)
+        page = self.paginate_queryset(active_notifications)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(active_notifications, many=True)
+        return Response(serializer.data)
